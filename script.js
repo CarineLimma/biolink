24cfe7c49939abafd50a4a751330b094a125783d
Carine Lima <carinesoaresdelima@gmail.com>
2025-10-24 14:45:46 -0300

    corrigindo erro
    corrigindo erro no modal

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-document.getElementById("abrirSobre").onclick = function() {
-  document.getElementById("modalSobre").style.display = "block";
-}
 
-document.querySelector(".fechar").onclick = function() {
-  document.getElementById("modalSobre").style.display = "none";
-}
+const botaoAbrir = document.getElementById("abrirSobre");
+const modal = document.getElementById("modalSobre");
+const botaoFechar = modal.querySelector(".fechar");
 
-window.onclick = function(event) {
-  if (event.target == document.getElementById("modalSobre")) {
-    document.getElementById("modalSobre").style.display = "none";
+botaoAbrir.addEventListener("click", () => {
+  modal.style.display = "flex";
+  document.body.style.overflow = "hidden"; 
+});
+
+botaoFechar.addEventListener("click", () => {
+  modal.style.display = "none";
+  document.body.style.overflow = "auto"; 
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto";
   }
-}
+});
